e390ad1fca59a80a71cd8d5d51ae3fb4140a3bae
Lorenzo Santini <lorenzo.santini@diesys.net>
2026-01-03 20:16:39 +0100

    agg

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+function toggleMenu() {
+    // Trova l'elemento sidebar
+    var sidebar = document.getElementById("mySidebar");
+    
+    // Se ha la classe "open", la toglie. Se non ce l'ha, la mette.
+    sidebar.classList.toggle("open");
+}
